d1e9136ccf9ea6fcd8c46a9727afd3439fd57811
00glitched <conaeze3@gmail.com>
2025-02-16 11:45:32 -0300

    Error: Selecting theme

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ cp $path/themes/glitchedGlassCyan.micro $HOME/.config/micro/colorschemes/glitche
 cp $path/themes/glitchedGlassMagenta.micro $HOME/.config/micro/colorschemes/glitchedGlassMagenta.micro
 read -p "Select a color Cyan/Magenta: " color
 if [ "$color" = "Cyan" ]; then
-  	echo "{\"colorscheme\" : \"GlitchedGlassCyan\"}" > $HOME/.config/micro/settings.json
+  	echo "{\"colorscheme\" : \"glitchedGlassCyan\"}" > $HOME/.config/micro/settings.json
   	echo "Color scheme changed to Cyan."
 elif [ "$color" = "Magenta" ]; then
-	echo "{\"colorscheme\": \"GlitchedGlassMagenta\"}" > $HOME/.config/micro/settings.json
+	echo "{\"colorscheme\": \"glitchedGlassMagenta\"}" > $HOME/.config/micro/settings.json
 	echo "Color scheme changed to Magenta."
 else
 	echo "Not valid. You can select manually with 'micro -colorscheme NAME' "
